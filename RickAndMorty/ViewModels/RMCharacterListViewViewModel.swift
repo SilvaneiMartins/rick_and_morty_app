fdea75a60bc8d3b593eb9f09828d2751cb3ae1bb
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-03-07 13:45:17 -0400

    App rick and morty character pagination indicator

--- RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -16,6 +16,7 @@ protocol RMCharacterListViewModelDelegate: AnyObject {
 final class RMCharacterListViewViewModel: NSObject {
     
     public weak var delegate: RMCharacterListViewModelDelegate?
+    private var isLoadingMoreCharacters = false
     
     private var characters: [RMCharacter] = [] {
         didSet {
@@ -56,11 +57,11 @@ final class RMCharacterListViewViewModel: NSObject {
     }
     
     public func fetchAdditionalCharacters() {
-        // Fetch characters
+        isLoadingMoreCharacters = true
     }
     
     public var shouldShowLoadMoreIndicator: Bool {
-        return apiInfo?.next != nil
+        return  apiInfo?.next != nil
     }
 }
 
@@ -77,6 +78,25 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
         }
         cell.configure(width: cellViewModels[indexPath.row])
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionFooter,
+            let footer = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier, for: indexPath
+            ) as? RMFooterLoadingCollectionReusableView else {
+            fatalError("Unsuppored")
+        }
+        footer.startAnimating()
+        return footer
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        guard shouldShowLoadMoreIndicator else {
+            return .zero
+        }
+        return CGSize(width: collectionView.frame.width, height: 100)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -99,8 +119,16 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
 
 extension RMCharacterListViewViewModel: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard shouldShowLoadMoreIndicator else {
+        guard shouldShowLoadMoreIndicator, !isLoadingMoreCharacters else {
             return
+        }
+        
+        let offset = scrollView.contentOffset.y
+        let totalContentHeight = scrollView.contentSize.height
+        let totalScrollViewFixedHeight = scrollView.frame.size.height
+        
+        if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
+            fetchAdditionalCharacters()
         }
     }
 }
